fd89700d6f32706d5b710eb793025278d2822f45
qiang.tang <qiang.tang@tradar.com>
2014-10-17 10:20:22 +0100

    Commit code

---
+++ PMSDreamNode/PMSDreamNode/apps/respository/datasource.js
@@ -0,0 +1,11 @@
+﻿function profile()
+{
+    return randomIntFromInterval(-100, 100);
+}
+
+function randomIntFromInterval(min,max)
+{
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+exports.profile=profile;
---
+++ PMSDreamNode/PMSDreamNode/apps/respository/datasourceCSharp.js
@@ -0,0 +1,23 @@
+﻿
+
+function profile() {
+    return temp=randomIntFromCSharp();
+}
+
+function randomIntFromCSharp()
+{
+    var edge=require('edge');
+
+    var clrMethod = edge.func({
+        assemblyFile: __dirname+'\\MockDataSourceConnection.dll',
+        typeName: 'MockDataSourceConnection.DataFactory',
+        methodName: 'GetData'
+        });
+    var returnvalue;
+    clrMethod(null, function(error, result) {
+        if(error) throw error;
+        returnvalue= parseInt(result);
+        });
+    return returnvalue;
+}
+exports.profile=profile;
